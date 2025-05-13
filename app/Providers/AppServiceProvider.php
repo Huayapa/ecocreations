a3e3d517264e678d2b54cebfef6193cfa21ebeae
josue Huayapa <huayapajosue0@gmail.com>
2025-05-12 19:03:59 -0500

    cmd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\View\Components\carritoCompras;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+        }
         Blade::component('carrito-compras', carritoCompras::class);
     }
 }
